a82a5a6eb43b037a40923a388c869b7a29cac9cd
Santiago Bejarano <bejaranosantiago77@gmail.com>
2026-05-05 20:58:46 -0500

    Segundo avance

--- Nomina.py
+++ Nomina.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 from ast import Dict
 
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import List, Dict   
+
 
 class cuentabancaria (ABC):
     def __init__(self, numero_cuenta: str, titular: str, saldo: float = 0):
@@ -13,3 +17,27 @@ class cuentabancaria (ABC):
     def calcular_comision(self, monto: float) -> float:
         """Metodo Abstracto: Cada tipo de cuenta tiene su propia comisión"""
         pass
+
+    @abstractmethod
+    def get_tipo_cuenta(self) -> str:
+        """Metodo Abstracto: Returna el tipo de cuenta (Polimorfismo)"""
+        pass
+
+    def depositar(self, monto: float) -> bool:
+        "Metodo heredado por todas las otras cuentas"
+        if monto > 0:
+            self._saldo += monto
+            self._registrar_transaccion("DEPOSITO", monto)
+            return True
+        return False
+
+    def retirar(self, monto: float) -> bool:
+        "Metodo heredado por todas las otras cuentas"
+        comision = self.calcular_comision(monto)
+        total_retiro = monto + comision
+
+        if total_retiro <= self._saldo:
+            self._saldo -= total_retiro
+            self._registrar_transaccion("RETIRO", monto, f"Comisión: {comision:,.0f}")
+            return True
+        return False
