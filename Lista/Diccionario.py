2a39ff1cb927a18b6c32cbca9abfc88977983c57
Santiago Bejarano <bejaranosantiago77@gmail.com>
2026-05-12 19:59:13 -0500

    Pop

--- Lista/Diccionario.py
+++ Lista/Diccionario.py
@@ -1,6 +1,10 @@
 ListaFrutas = ["Limon", "Fresa", "Manzana", "Mango", "Durazno ","Naranja"]
 
-ListaFrutas.append("Mandarina")
+ListaFrutas.append("Mandarina")    
+
+ListaFrutas[5] = "Sandia"
 
 for fruta in ListaFrutas:
     print(f"fruta: {fruta}")
+
+ListaFrutas.pop(3)      
